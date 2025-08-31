598e414dff720ecea98d2eb6c8cc438e46c2f19e
misProyectosPropios <inakifrutos@outlook.com>
2025-08-31 19:43:41 -0300

    Create code.c

---
+++ GuiaC/avanzado/debugging/folder1/code.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+typedef struct{
+  char nombre[5];
+  uint8_t habilitado; // bool
+  int saldo;
+  int gustoDeHeladoFavorito;
+} usuario_t;
+
+void habilitarUsuario(usuario_t *usuario){
+  usuario->habilitado = 1;
+  for(int i = 0; i <= 5; i++){
+  usuario->nombre[i] = '\0';
+  }
+  usuario->saldo = 0; 
+  }
+
+int main(void){
+  usuario_t *nuevoUsuario = malloc(sizeof(usuario_t));
+  habilitarUsuario(nuevoUsuario);
+  if(nuevoUsuario->habilitado){
+    printf("Usuario habilitado con ´exito!\n");
+  } else{
+    printf("Error al habilitar usuario.\n");
+  }
+  free(nuevoUsuario);
+  return 0;
+  }
