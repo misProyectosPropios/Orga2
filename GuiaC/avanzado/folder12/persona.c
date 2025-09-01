260f10c2c07d6b1bd8a340df752cb97961a39dad
misProyectosPropios <inakifrutos@outlook.com>
2025-09-01 12:01:43 +0000

    Fix bug avanzado/12

--- GuiaC/avanzado/folder12/persona.c
+++ GuiaC/avanzado/folder12/persona.c
@@ -29,5 +29,6 @@ int main() {
     struct persona_t* persona_dir = crearPersona("Favaloro", 77);
     printf("La persona: nombre: %s, Edad: %d", persona_dir->nombre, persona_dir->edad);
 
+    free(persona_dir->nombre);
     free(persona_dir);    
 }
