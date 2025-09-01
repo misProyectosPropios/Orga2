4949d0f77d3b36833b7e84b21e0d74fdc6c69162
misProyectosPropios <inakifrutos@outlook.com>
2025-09-01 11:56:10 +0000

    Exercise 11. Add files

---
+++ GuiaC/avanzado/folder11/secuencia.c
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+uint16_t *secuencia(uint16_t n){
+    uint16_t arr[n];
+    for(uint16_t i = 0; i < n; i++)
+        arr[i] = i;
+    return arr;
+}
---
+++ GuiaC/avanzado/folder11/secuencia.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+
+uint16_t *secuencia(uint16_t n);
